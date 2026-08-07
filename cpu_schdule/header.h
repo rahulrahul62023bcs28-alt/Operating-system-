90348999fa0231d2081c7f07813c954884652886
Rahul R <rahulrahul62023_bcs28@mepcoeng.ac.in>
2026-08-07 11:28:18 +0530

    Add header file for CPU scheduling functions

---
+++ cpu_schdule/header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void display(int id[], int run[], int wait[], int turn[], int n);
+
+void fcfs(int id[], int run[], int n);
+
+void sjf(int id[], int run[], int n);
+
+void rr(int id[], int run[], int n);
